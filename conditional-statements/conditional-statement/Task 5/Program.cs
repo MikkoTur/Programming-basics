52692bd72f8cc46c0a40a853a7f81b8a23f49990
MikkoTur <mikko.turppo@hotmail.com>
2017-10-05 10:31:24 +0300

    loop_example

---
+++ conditional-statements/conditional-statement/Task 5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace loop_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma laskee. Anna luku, jonka kertoma lasketaan:");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            //int.TryParse(userInput, out int number);
+            int i = 0;
+            int f = 1;
+            do
+            {                
+                i = i + 1;
+                f = f * i;
+                //Console.WriteLine($"{i}!={f}"); // 5!=120
+            } while (i < number);
+
+            Console.WriteLine($"Syötit {i}\n vastaus {f}"); // 5!=120
+            Console.ReadKey();
+        }
+    }
+}
