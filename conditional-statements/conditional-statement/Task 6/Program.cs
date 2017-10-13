2a2dbedfeea2fb14db43355c7acb2615925b33e5
MikkoTur <mikko.turppo@hotmail.com>
2017-10-13 10:25:25 +0300

    task6&7 done

---
+++ conditional-statements/conditional-statement/Task 6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma laskee. Anna luku, mistä summa lasketaan:");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            //int.TryParse(userInput, out int number);
+            int i = 0;
+            int num = 0;
+            while (i < number)
+            {
+                i = i + 1;
+                num = num + i;
+                
+            } 
+
+            Console.WriteLine($"lukujen summa: {num}"); 
+            Console.ReadKey();
+        }
+    }
+}
---
+++ conditional-statements/conditional-statement/Task 7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma laskee ensimmäisen parillisen ja parittoman lukujen summan:");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            //int.TryParse(userInput, out int number);
+            int i = 0;
+            int parillinen = 0;
+            int pariton = 0;
+            while (i <= number)
+            {
+                if(i % 2 == 0)
+                {
+                    parillinen = parillinen + i;
+                }
+                else
+                {
+                    pariton = pariton + i;
+                }
+                i = i + 1;
+                
+            }
+
+            Console.WriteLine($"lukujen summa: {parillinen} ja {pariton}");
+            Console.ReadKey();
+        }
+    }
+}
