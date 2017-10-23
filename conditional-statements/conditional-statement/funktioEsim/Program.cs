fe5327f5edeac2b8d62f5c618ce25014968288dd
MikkoTur <mikko.turppo@hotmail.com>
2017-10-23 10:54:10 +0300

    do,while,for DONE

--- conditional-statements/conditional-statement/funktioEsim/Program.cs
+++ conditional-statements/conditional-statement/funktioEsim/Program.cs
@@ -6,7 +6,33 @@ namespace funktioEsim
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            bool isNumber;
+            int N;
+            int sum = 0;
+
+            Console.WriteLine("Lasketaan N ensimmäistä lukua yhteen");
+            Console.WriteLine("Syötä N: ");
+
+            if (isNumber = int.TryParse(Console.ReadLine(), out N))
+            {
+                if (N > 0)
+                {
+                    for (int i = 1; i <= N; i++)
+                    {
+                        sum = sum + i;
+                    }
+
+                    Console.WriteLine($"Lukujen summa on: {sum}");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Syötit muuta kuin numeron");
+                Console.ReadKey();
+            }
         }
     }
 }
