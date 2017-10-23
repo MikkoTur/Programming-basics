848fff1e77f709b4ade95a024cd59afc5297c7b1
MikkoTur <mikko.turppo@hotmail.com>
2017-10-23 11:32:49 +0300

    task 3.1/3.2

---
+++ conditional-statements/conditional-statement/Task3.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa kokonaislukujen 1-10 neliojuuret");
+
+            for (int f = 1; f <= 10; f++)
+            {
+                Console.WriteLine($"{f}\t\t{Math.Round(Math.Sqrt(f), 2)}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ conditional-statements/conditional-statement/Task3.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa kertotaulun luvuille 1-9");
+            int number = 1;
+            int sum;
+            int multip = 1;
+
+            for (number = 1; number <= 9; number++)
+            {
+                for (multip = 1; multip <= 9; multip++)
+                {
+                    sum = number * multip;
+                    Console.WriteLine($"{number} x {multip} = {sum}");
+                }
+            }
+            Console.ReadKey();
+
+            /*
+             while(number <= 9)
+             {
+             do shit
+             while(multip <=9)
+             {
+                do shit 2
+                multip++;
+             }
+             number++;
+             multip = 1;
+             }
+             */
+        }
+    }
+}
