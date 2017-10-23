ba7ba389c5b143b5a8351fc7cdb36a67f01f0f91
MikkoTur <mikko.turppo@hotmail.com>
2017-10-23 12:05:59 +0300

    task 3.123456 Done

---
+++ conditional-statements/conditional-statement/Task3.5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ohjelma tulostaa vakioveikkaus rivin (13 kohdetta) ");
+            Console.Write("1 = kotivoitto, X = tasapeli, 2 = vierasvoitto. \n");
+
+            Random rnd = new Random();
+            double rndNumber = 0.0;
+
+            for (int i = 1; i<=13;i++)
+            {
+                rndNumber = rnd.NextDouble();
+                if (rndNumber < 0.4)
+                {
+                    Console.WriteLine($"{i}. 1");
+                }
+                else if (rndNumber > 0.4 && rndNumber < 0.6)
+                {
+                    Console.WriteLine($"{i}. X");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}. 2");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ conditional-statements/conditional-statement/Task3.6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task3._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ohjelma simuloi nopanheittoa 1000 kertaa.\n");
+            Console.Write("Lisäksi ohjelma kertoo numeron 6 esiintymiskertojen lukumäärän.\n");
+
+            Random rnd = new Random();
+            int six = 0;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                Console.WriteLine($"{i}. {rnd.Next(6)}");
+                if (rnd.Next(6) == 1)
+                {
+                    six += 1;
+                }
+            }
+            Console.WriteLine($"\nKuutonen arvottiin {six} kertaa.");
+            Console.ReadKey();
+        }
+    }
+}
