62a2cfb360737880276a96bbb870d1fc44dc51bd
MikkoTur <mikko.turppo@hotmail.com>
2017-10-23 10:45:58 +0300

    Task 9 and 10 finished

---
+++ conditional-statements/conditional-statement/Task10/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            bool isNumber;
+            int N;
+            int Even = 0;
+            int Odd = 0;
+
+            Console.WriteLine("Lasketaan N:n ensimmäisten parillisten ja parittomien lukujen summat.");
+            Console.WriteLine("Syötä N: ");
+
+            if (isNumber = int.TryParse(Console.ReadLine(), out N))
+            {
+                if (N > 0)
+                {
+                    for (int i = 0; i <= N; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            Even = Even + i;
+                        }
+                        else if (i % 2 == 1)
+                        {
+                            Odd = Odd + i;
+                        }
+                    }
+                }
+                else if (N < 0)
+                {
+                    for (int i = 0; i >= N; i--)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            Even = Even + i;
+                        }
+                        else
+                        {
+                            Odd = Odd + i;
+                        }
+                    }
+                }
+                Console.WriteLine($"Parillisten summa on {Even} ja parittomien {Odd}");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Syötit muuta kuin numeron");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ conditional-statements/conditional-statement/Task9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            bool isNumber;
+            int N;
+            int sum = 0;
+
+            Console.WriteLine("Lasketaan N ensimmäistä lukua yhteen myös negatiivisella luvulla");
+            Console.WriteLine("Syötä N: ");
+
+            if (isNumber = int.TryParse(Console.ReadLine(), out N))
+            {
+                if (N > 0)
+                {
+                    for (int i = 1; i <= N; i++)
+                    {
+                        sum = sum + i;
+                    }
+                }
+                else if (N < 0)
+                {
+                    for (int i = 0; i >= N; i--)
+                    {
+                        sum = sum + i;
+                    }
+                }
+
+                    Console.WriteLine($"Lukujen summa on: {sum}");
+                    Console.ReadKey();               
+            }
+            else
+            {
+                Console.WriteLine("Syötit muuta kuin numeron");
+                Console.ReadKey();
+            }
+        }
+    }
+}
