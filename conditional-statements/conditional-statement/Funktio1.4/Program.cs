8c1de4a56714f14a7e4c14b6debb62c38a2cfa60
MikkoTur <mikko.turppo@hotmail.com>
2017-10-24 10:50:01 +0300

    Funktio 1.4

---
+++ conditional-statements/conditional-statement/Funktio1.4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Funktio1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Suurin luku oli " + numerot());
+            Console.ReadKey();
+        }
+        static int numerot()
+        {
+            int num = 0;
+            int max = 0;
+            bool isNumber;
+            Console.WriteLine("Syötä 10 positiivista lukua");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i + 1}. ");
+
+                if (isNumber = int.TryParse(Console.ReadLine(), out num) && num > 0)
+                {
+                    if (max < num)
+                    {
+                        max = num;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Väärä syöte, syötä positiivinen luku");
+                    i--;
+                }
+            }
+            return max;
+        }
+    }
+}
