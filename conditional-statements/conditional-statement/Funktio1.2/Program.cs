3ce6d863b37a5703cab206649adf1f2291f51084
MikkoTur <mikko.turppo@hotmail.com>
2017-10-24 10:18:34 +0300

    Funktio 2.1 done

---
+++ conditional-statements/conditional-statement/Funktio1.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Funktio1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            int x;
+            int y;
+
+            Console.WriteLine("Syötä ensimmäinen luku: ");
+            int.TryParse(Console.ReadLine(), out x);
+            Console.WriteLine("Syötä toinen luku: ");
+            int.TryParse(Console.ReadLine(), out y);
+
+            Console.WriteLine($"Pienempi luku on: {minimi(x, y)}");
+            Console.ReadKey();
+        }
+        static int minimi(int x, int y)
+        {
+            int min;
+            if (x > y)
+            {
+                min = y;
+            }
+            else
+            {
+                min = x;
+            }
+            return min;
+        }
+    }
+}
