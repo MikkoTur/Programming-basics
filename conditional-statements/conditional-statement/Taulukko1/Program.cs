8f3487a5513e2854d518f23e64616e33a5aab962
MikkoTur <mikko.turppo@hotmail.com>
2017-11-02 13:32:46 +0200

    Taulukko 1

---
+++ conditional-statements/conditional-statement/Taulukko1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Taulukko1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma arpoo 100 lukua väliltä 0-50 ja muodostaa niistä taulukon.");
+            int[] arrayNumber = new int[100];
+            Random rnd = new Random();
+            double sum = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                arrayNumber[i] = rnd.Next(0, 51);
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"{i}. {arrayNumber[i]}");
+                sum += arrayNumber[i]; 
+            }
+            Console.WriteLine($"Lukujen summa on {sum}");
+            Console.WriteLine($"Lukujen keskiarvo on {sum / 100}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ conditional-statements/conditional-statement/Tähtitask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tähtitask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
