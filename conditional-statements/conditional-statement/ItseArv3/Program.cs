cb40110b836d6f19465f0a545a1b834e09c57116
MikkoTur <mikko.turppo@hotmail.com>
2017-11-09 08:51:09 +0200

    Itsearviointi done

---
+++ conditional-statements/conditional-statement/ItseArv3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ItseArv3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int summa = 0;
+            int userInput = 0;
+            bool isNumber = true;
+            Console.WriteLine("Syötä luku. -1 pysäyttää ohjelman ja tulostaa summan");
+
+            while (isNumber)
+            {
+                Console.Write("Luku: ");
+                isNumber = int.TryParse(Console.ReadLine(), out userInput);
+
+                if (userInput == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    summa += userInput;
+                }
+            }
+            Console.WriteLine($"Lukujen summa on: {summa}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ conditional-statements/conditional-statement/Lottoteht/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lottoteht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
