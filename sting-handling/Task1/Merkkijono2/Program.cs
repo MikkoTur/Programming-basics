bb937f116d71e9547f6ebc1fa2e5d87329e67dca
MikkoTur <mikko.turppo@hotmail.com>
2017-11-23 09:39:29 +0200

    merkkijono2 done

---
+++ sting-handling/Task1/Merkkijono2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Merkkijono2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma muuttaa e kirjaimet @ merkiksi");
+            string e = Console.ReadLine();
+            e = e.Replace('e', '@');
+            Console.WriteLine($"Lopullinen virkkeesi: '{e}'");
+            Console.ReadKey();
+        }
+    }
+}
