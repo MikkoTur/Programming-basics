7b1566c621849dc849ebaa1accb95e12d43bf057
MikkoTur <mikko.turppo@hotmail.com>
2017-11-23 09:11:35 +0200

    merkkijono1 done

---
+++ sting-handling/Task1/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma ilmoittaa merkkien lukumäärän");
+            string merkkijono = Console.ReadLine();
+            int length = merkkijono.Length;
+            Console.WriteLine($"Syötteessä on {length} merkkiä");
+            Console.ReadKey();
+
+        }
+    }
+}
