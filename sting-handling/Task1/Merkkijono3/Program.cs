3d7060364767a0a66ea4b893403a7578c6e6f336
MikkoTur <mikko.turppo@hotmail.com>
2017-11-30 10:36:41 +0200

    Merkkijonot done

---
+++ sting-handling/Task1/Merkkijono3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Merkkijono3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Syötä virke ohjelma laskee L kirjainten lukumäärän");
+            string Userinput = Console.ReadLine().ToUpper();
+
+            int i = 0;
+            foreach(char x in Userinput)
+            {
+                if (x == 'L')
+                    i++;
+            }
+            Console.WriteLine($"Tekstissä on {i} L-kirjainta! ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sting-handling/Task1/Merkkijono4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Merkkijono4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma tarkastaa onko sana palintromi");
+            string UserInput = Console.ReadLine().ToUpper();
+            string reverseUserInput = "";
+
+            for (int i = UserInput.Length - 1; i >= 0; i--)
+            {
+                reverseUserInput += UserInput[i];
+            }
+            if (UserInput.Replace (" ", "") == reverseUserInput.Replace (" ", ""))
+            {
+                Console.WriteLine($" Syöttämäsi teksti, {UserInput.ToLower()} on palintromi");
+            }
+            else
+            {
+                Console.WriteLine($" Syöttämäsi teksti, {UserInput.ToLower()} ei ole palintromi");
+            }
+            Console.ReadKey();
+        }
+    }
+}
