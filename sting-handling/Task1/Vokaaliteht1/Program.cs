9fa3a9b70f7d11eaf2454f77f502763fd73aa697
MikkoTur <mikko.turppo@hotmail.com>
2017-11-30 11:26:01 +0200

    Vokaalitehtava done

---
+++ sting-handling/Task1/Vokaaliteht1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vokaaliteht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma laskee virkkeesi vokaalien lukumäärän");
+            string userInput = Console.ReadLine().ToLower();
+            int MP = userInput.Length;
+
+            int count = 0;
+
+            foreach(char ch in userInput)
+            {
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'y' || ch == 'ä' || ch == 'ö')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Sanassa {userInput} on {count} vokaalia");
+            Console.ReadKey();
+        }
+    }
+}
